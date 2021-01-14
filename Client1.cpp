87b8b2db5a319cbe6676001f9b3b793ba144636a
LievRenard <68917666+LievRenard@users.noreply.github.com>
2021-01-14 21:59:44 +0900

    Update Client1.cpp

--- Client1.cpp
+++ Client1.cpp
@@ -3,6 +3,9 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <fstream>
+#include <vector>
+using namespace std;
 
 class Socket {
 private:
@@ -19,21 +22,41 @@ public:
         
         sock = socket(AF_INET, SOCK_STREAM, 0);
         if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) == -1){
-            std::cout << "Connection Error" << std::endl;
+            cout << "Connection Error" << endl;
         }
     }
 };
+
+char* convChar(string str){
+    vector<char> vc(str.begin(), str.end());
+    vc.push_back('\0');
+    char* c = &*vc.begin();
+    return c;
+}
 
 int main(int argc, const char * argv[]) {
     unsigned long int ip = inet_addr("203.227.140.199");
     Socket server = Socket(ip, 9966);
     
+    /*
     char msg[] = "Hello world\n";
     write(server.sock,msg,sizeof(msg));
+     */
+    
+    ifstream ReadFile;
+    ReadFile.open("/Users/evan/Documents/PyVacation/Ref/IdealGasPV.txt");
+    
+    if (ReadFile.is_open()){
+        string tmp;
+        getline(ReadFile,tmp);
+        char* msg = convChar(tmp);
+        
+        write(server.sock,msg,sizeof(tmp));
+    }
     
     char message[1024] = {0x00,};
     read(server.sock,message,sizeof(message)-1);
-    std::cout << message << std::endl;
+    cout << message << endl;
     
     close(server.sock);
     return 0;
