cbbb387b64fd4c6b88ece967c39672c28c7fd23e
LievRenard <68917666+LievRenard@users.noreply.github.com>
2021-01-14 20:21:36 +0900

    Update Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -1,35 +1,40 @@
-//
-//  main.cpp
-//  RPG_Socket
-//
-//  Created by 이정호 on 2021/01/14.
-//
-
 #include <iostream>
 #include <string>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 
-int main(int argc, const char * argv[]) {
+class Socket {
+private:
     struct sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr("203.227.140.199");
-    addr.sin_port = htons(9966);
     
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) == -1){
-        std::cout << "Connection Error" << std::endl;
+public:
+    int sock;
+    
+    Socket(unsigned long int ip,int port){
+        memset(&addr, 0, sizeof(addr));
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = ip;
+        addr.sin_port = htons(port);
+        
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (connect(sock, (struct sockaddr*)&addr, sizeof(addr)) == -1){
+            std::cout << "Connection Error" << std::endl;
+        }
     }
+};
+
+int main(int argc, const char * argv[]) {
+    unsigned long int ip = inet_addr("203.227.140.199");
+    Socket server = Socket(ip, 9966);
     
     char msg[] = "Hello world\n";
-    write(sock,msg,sizeof(msg));
+    write(server.sock,msg,sizeof(msg));
     
     char message[1024] = {0x00,};
-    read(sock,message,sizeof(message)-1);
+    read(server.sock,message,sizeof(message)-1);
     std::cout << message << std::endl;
     
-    close(sock);
+    close(server.sock);
     return 0;
 }
