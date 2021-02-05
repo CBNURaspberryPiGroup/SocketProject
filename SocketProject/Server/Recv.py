e41ae2483ceb7b6df61f683546b633053c479494
gominsung <77944441+gominsung@users.noreply.github.com>
2021-02-05 22:13:04 +0900

    Update Recv.py

--- SocketProject/Server/Recv.py
+++ SocketProject/Server/Recv.py
@@ -6,6 +6,14 @@ class RecvData:
         self.client = client
         self.stroage = storage
 
+    import socket
+from PIL import Image
+
+class RecvData:
+    def __init__(self,client,storage):
+        self.client = client
+        self.stroage = storage
+
     def recv_img(self,fn):
         try:
             matadata= self.client.recv()
@@ -24,11 +32,12 @@ class RecvData:
             img_data+=data
 
             data = Image.frombytes(size,img_mode,img_data) #(size,mode,data)
-            data.save(%s+%s) % self.stroage %fn
+            data.save("%s+%s") % self.stroage %fn
 
         except Exception as e:
             print(e)
             self.client.send(e)
+
 
     def recv_txt(self,fn)
          try:
