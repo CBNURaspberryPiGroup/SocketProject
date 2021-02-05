8a3f0db3e210a42597b8e4a42481bd64650bf136
LievRenard <68917666+LievRenard@users.noreply.github.com>
2021-02-05 20:26:39 +0900

    Update Identification.py

--- SocketProject/Server/Identification.py
+++ SocketProject/Server/Identification.py
@@ -6,18 +6,60 @@ class Identification:
     def __init__(self,client):
         self.client = client
         if not exists('Key.key'):
-            self.key = fnet.generate_key()
-            print(self.key)
+            self.__key = fnet.generate_key()
+            print(self.__key)
             with open('Key.key','w') as f:
-                f.write(str(self.key))
+                f.write(str(self.__key))
         else:
             with open('Key.key','r') as f:
-                self.key = byte(f.readlines())
+                self.__key = f.readlines().encode()
+        self.__crypt = Fernet(self.__key)
+        self.DatabaseOpen()
+    
+    def DatabaseOpen(self):
         if not exists('Auth.Data'):
             with open('Auth.Data','w'):
                 pass
+        with open('Auth.Data','r') as f:
+            self.__Base = {}
+            try:
+                while(True):
+                    self.__Base[f.readlines()] = f.readlines()
+            except Exception:
+                pass
+                
+    def DatabaseWrite(self):
+        if exists('Auth.Data'):
+            with open('Auth.Data','w') as f:
+                for id in self.__Base.keys():
+                    f.write(id)
+                    f.write(self.__Base[id])
         
     def Authentification(self):
-        client.sendall('ID'.encode())
-        with open('Auth.Data','r'):
+        self.client.sendall('ID:'.encode())
+        id = self.client.recv()
+        if not id in self.__Base:
+            self.client.sendall('ID Not Found'.encode())
+            return False
+        self.client.sendall('Password:'.encode())
+        passwd = self.client.recv()
+        if password != self.__crypt.decrypt(self.__Base[id]):
+            self.client.sendall('Wrong Password'.encode())
+            return False
+        return True
             
+    def Register(self):
+        self.client.sendall('Register: ID:'.encode())
+        id = self.client.recv()
+        if id in self.__Base :
+            self.client.sendall('ID Already Exists'.encode())
+            return False
+        self.client.sendall('Password:'.encode())
+        passwd = self.client.recv()
+        self.client.sendall('Password Confirm:')
+        if self.client.recv() != passwd:
+            self.client.sendall('Password Confirm Failed'.encode())
+            return False
+        self.__Base[id] = self.__crypt.encrypt(passwd)
+        self.DatabaseWrite()
+        return True
