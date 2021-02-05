912fc57ca004395626cd80aaa5a7b4b2a3e72e84
gominsung <77944441+gominsung@users.noreply.github.com>
2021-02-05 21:19:27 +0900

    Update recv data.py

--- SocketProject/Server/recv data.py
+++ SocketProject/Server/recv data.py
@@ -9,22 +9,22 @@ class RecvData:
     def recv_img(self,fn):
         try:
             matadata= self.client.recv()
-            matadata=matadata.split()
+            matadata= matadata.split("|")
             img_size= matadata[1].split(",")
-            img_size="int(img_size[1][1:]), int(img_size[2][1:])"
+            size="%i,%i" %img_size[1][1:] %img_size[2][1:]
         
             img_mode=matadata[3]
         
             img_data=""
             data = self.client.recv(1024) 
-            while len(data)==1024 :
+            while len(data)==1024 : 
                 img_data+=data
                 data=self.client.recv(1024)
             data=self.cliemt.recv(1024)
             img_data+=data
 
-            data = Image.frombytes(img_size,img_mode,img_data) #(size,mode,data)
-            data.save("cliet.stroage+fn")
+            data = Image.frombytes(size,img_mode,img_data) #(size,mode,data)
+            data.save("%s/+fn") % self.stroage
 
         except Exception as e:
             print(e)
@@ -33,3 +33,11 @@ class RecvData:
     def recv_txt(self,fn)
          try:
             f=open("cliet.stroage+fn",'w')
+            while True:
+            data=self.client.recv()
+            f.write(data)
+            
+            except Exception as e:
+            print(e)
+            self.send(e)
+
