c2a896b281ef41668ab2592da972648c3779aadf
LievRenard <68917666+LievRenard@users.noreply.github.com>
2021-02-09 21:34:54 +0900

    add Mode Conversion

--- SocketProject/Server/Send.py
+++ SocketProject/Server/Send.py
@@ -27,6 +27,9 @@ class SendData:
     def send_img(self,fn):
         try:
             data = Image.open(self.storage+"/"+fn)
+            if '.jpg' in fn[-4:] : modeConv = 'RGB'
+            else '.png' in fn[-4:] : modeConv = 'RGBA'
+            data = data.convert(mode=modeConv)
             metadata = "Size:%s:Mode:%s"%(data.size,data.mode)
             data = data.tobytes()
             self.send(metadata.encode())
