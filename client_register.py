5d5134ca9b724f5b120399a1256c2108449f655d
ji-hyun-hwang <77057706+ji-hyun-hwang@users.noreply.github.com>
2021-02-16 22:35:24 +0900

    print("고민성 고추")

--- client_register.py
+++ client_register.py
@@ -2,79 +2,89 @@ from socket import socket
 import msvcrt
 import sys
 
+
 class client_id :
-    def Idmodify (self) :
-        self.server.recv(1024)
+    def __init__ (self, client) :
+        self.client = client
+
+    def Id_login (self) :
+        id = self.client.recv(1024)
+        Deid = id.decode()
         print("ID를 입력하세요.")
-        ID = input("ID를 입력하세요.:")
-        ID_list = ID.split()
-        if " " in ID : print("공백은 사용할 수 없습니다.")
-        elif len(ID_list) >= 8 : print("ID는 8자 이하로 입력하세요")
-        elif len(ID_list) <= 3: print("ID는 3자 이상 입력하세요")
-        print("ID 처리 완료")
-        self.server.sendall(ID)
-
-        
-    def pwmodify(prompt = "Password를 입력하세요:"):
-        self.server.recv(1024)
-        sys.stdout.write(prompt)
-        sys.stdout.flush()
-        buf = bytearray()
-        skip = False
-        while True:
-            if not msvcrt.kbhit() : continue
-            x: bytes = msvcrt.getch()
-            if x.startswith(b"\xe0") or x.startswith(b"\x00"):
-                skip = True
-                continue
-            if skip:
-                skip = False
-                continue
-            n = int.from_bytes(x, sys.byteorder)
-            if n == 13:
-                #Enter
-                msvcrt.putch('\n'.encode())
-                return buf.decode()
-            elif n == 8 :
-                if buf:
-                    sys.stdout.write("\b \b")
-                    sys.stdout.flush()
-                    buf.pop()
-            elif 32 <= n < 126:
-                msvcrt.putch('*'.encode())
-                buf.append(n)
-        self.server.sendall(Password)
-
-    if __name__ == "__main__":
-        print(get_password()) 
-        
-    def register_id(self) :
-        self.server.recv(1024)
-        print("등록하고 싶은 ID를 입력하세요.")
-        ID = input("ID를 입력하세요.:")
-        ID_list = ID.split()
-        if " " in ID : print("공백은 사용할 수 없습니다.")
-        elif len(ID_list) >= 8 : print("ID는 8자 이하로 입력하세요")
-        elif len(ID_list) <= 3: print("ID는 3자 이상 입력하세요")
-        print("ID 처리 완료")
-        self.server.sendall(ID)
-
-    def register_Pw(self) :
-        self.server.recv(1024)
-        print("등록하고 싶은 Password를 입력하세요.")
-        Password = input("Password를 입력하세요.:")
-        Password_list = Password.split()
-        if " " in Password : print("공백은 사용할 수 없습니다.")
-        elif len(Password_list) >= 6 : print("ID는 6자 이하로 입력하세요")
-        elif len(Password_list) <= 4: print("ID는 4자 이상 입력하세요")
         
         while True :
-            Password2 = input("Password를 한번 더 입력하세요.:")
-            if " " in Password : print("공백은 사용할 수 없습니다.")
-            elif len(Password_list) >= 6 : print("ID는 6자 이하로 입력하세요")
-            elif len(Password_list) <= 4: print("ID는 4자 이상 입력하세요")
+            ID = input(Deid)
+            ID_list = ID.split()
+            if " " in ID : print("공백은 사용할 수 없습니다.")
+                True
+            elif len(ID_list) >= 8 : print("ID는 8자 이하로 입력하세요")
+                True
+            elif len(ID_list) <= 3: print("ID는 3자 이상 입력하세요")
+                True
+            else : False
+        print("ID 처리 완료")
+        self.client.sendall(ID.encode)
 
-            if Password == Password2 :
-                self.server.sendall(Password2) 
+        
+    def pw_login(self) :
+        pw = self.client.recv(1024)
+        depw = pw.decode()
+        print("pw를 입력하세요.")
+            
+        while True :
+            pw = input(depw)
+            pw_list = pw.split()
+                if " " in  : print("공백은 사용할 수 없습니다.")
+                    True
+                elif len(pw_list) >= 8 : print("pw는 8자 이하로 입력하세요")
+                    True
+                elif len(pw_list) <= 3: print("pw는 3자 이상 입력하세요")
+                    True
+                else : False
+            print("pw 처리 완료")
+            self.client.sendall(pw.encode())
+        
+    def register_id(self, client) :
+        id = self.client.recv(1024)
+        deid = id.decode()
+        print("등록하고 싶은 ID를 입력하세요.")
+        while True :
+            ID = input(Deid)
+            ID_list = ID.split()
+            if " " in ID : print("공백은 사용할 수 없습니다.")
+                True
+            elif len(ID_list) >= 8 : print("ID는 8자 이하로 입력하세요")
+                True
+            elif len(ID_list) <= 3: print("ID는 3자 이상 입력하세요")
+                True
+            else : False
+        print("ID 처리 완료")
+        self.client.sendall(ID.encode)
+
+    def register_Pw(self, client) :
+        pw = self.client.recv(1024)
+        depw = pw.decode()
+        print("등록하고 싶은 Password를 입력하세요.")
+        while True :
+            pw = input(depw)
+            pw_list = pw.split()
+                if " " in  : print("공백은 사용할 수 없습니다.")
+                    True
+                elif len(pw_list) >= 8 : print("pw는 8자 이하로 입력하세요")
+                    True
+                elif len(pw_list) <= 3: print("pw는 3자 이상 입력하세요")
+                    True
+                else : False
+        pw = self.client.recv(1024)
+        depw2 = pw.decode()
+        while True :
+            depw == input(depw2)
+            pw_list = pw.split()
+            if " " in depw2 : print("공백은 사용할 수 없습니다.")
+            elif len(Pw_list) >= 6 : print("ID는 6자 이하로 입력하세요")
+            elif len(Pw_list) <= 4: print("ID는 4자 이상 입력하세요")
+
+            if depw == depw2 :
+                self.client.sendall(depw2) 
                 break
             else : print("첫번째 비밀번호와 두번째 비밀번호가 일치하지 않습니다.")
