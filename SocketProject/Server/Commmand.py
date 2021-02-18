442cb2b08c0a56fd251a7825298e74ce53caad25
wlans01 <76804251+wlans01@users.noreply.github.com>
2021-02-18 21:28:29 +0900

    로그인 커맨드 구현

--- SocketProject/Server/Commmand.py
+++ SocketProject/Server/Commmand.py
@@ -4,6 +4,7 @@ from os.path import exists
 import sys
 import Send
 import Recv
+import ldentification
 
 
 
@@ -26,8 +27,8 @@ class Command():
         self.split_f = filename.split(' ')  
         
         if self.split_f[0] == 'push':
-            if exists(self.split_f[1]):
-                err = ('Error -'+self.split_f[1]+'- 이미 존재하는 파일 입니다') 
+            if not exists(self.split_f[1]):
+                err = ('Error -'+self.split_f[1]+'- 존재하지 않는 파일 입니다') 
                 self.client.sendall(err.encode('utf-8'))
                 
                 print(err)
@@ -47,6 +48,24 @@ class Command():
             err = ('Error -'+self.split_f[0]+'- 존재하지 않는 명령어 입니다')
             print(err) 
             self.client.sendall(err.encode('utf-8'))
+    
+    def split2(self):
+        logein = self.client.recv(1024)
+        logein = logein.decode()
+        id=ldentification.ldentification(self.client)
+        
+        
+        if logein == 'y':
+            id.Authentification()
+            
+        if logein == 'n':
+            id.Register()    
+            
+            
+            
+        
+    
+    
                   
     def file_push(self):
         filename, fileExtension = os.path.splitext(self.split_f[1])
@@ -59,14 +78,6 @@ class Command():
         elif fileExtension == '.png'or'.jpg':
             Rec.recv_img(self.split_f[1]) 
         
-        
-        
-        
-        
-            
-            
-            
-
               
     def file_pull(self):
         filename, fileExtension = os.path.splitext(self.split_f[1])
