c897a425e2b965b1947174b2eee2039f30c3f4ac
gominsung <77944441+gominsung@users.noreply.github.com>
2021-02-18 20:34:31 +0900

    Update Recv.py

--- SocketProject/Server/Recv.py
+++ SocketProject/Server/Recv.py
@@ -30,11 +30,11 @@ class RecvData:
         f=open("%s%s"%(self.storage,fn),'w')
         while True :
             data=self.client.recv(1024)
-            if '\0'.decode() in data:
+            if '\0' in data.decode():
                 f.write(data.decode()[0:-1])
                 break
             else :
-                f.write(data.decode())    
+                f.write(data.decode())     
             
             
         
