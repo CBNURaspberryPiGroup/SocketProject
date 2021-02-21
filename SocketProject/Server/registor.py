dafd64b2462bbf098eb0769a87fd96e6dca4b06d
wlans01 <76804251+wlans01@users.noreply.github.com>
2021-02-21 16:56:54 +0900

    while문을 Commmand.py로 이동

--- SocketProject/Server/registor.py
+++ SocketProject/Server/registor.py
@@ -42,45 +42,48 @@ class Identification:
         print("Database Saved")
         
     def Authentification(self):
-        while True : 
-            self.client.sendall('ID:'.encode())
+         
+            
             id = self.client.recv(1024)
             if not( id in self.__Base):
                 self.client.sendall('ID Not Found'.encode())
-            else : break
-        while True :
-            self.client.sendall('Password:'.encode())
+                return
+            
+      
+            
             passwd = self.client.recv(1024)
             if passwd != self.__crypt.decrypt(self.__Base[id]):
                 self.client.sendall('Wrong Password'.encode())
-            else : 
-                print("Authentification Finished")
-                break
-            
+                return
+           
+            else:
+                return True
                 
             
     def Register(self):
-        while True :
-            self.client.sendall('Register: ID:'.encode())
+        
+            
             id = self.client.recv(1024)
             if id in self.__Base :
                 self.client.sendall('ID Already Exists'.encode())
+                return
             else : 
                 self.client.sendall('ID is available'.encode())
-                break
+                
             
         
-        while True :
-            self.client.sendall('Password:'.encode())
+    
+            
             passwd = self.client.recv(1024)
             self.client.sendall('Password Confirm:'.encode())
             if self.client.recv(1024) != passwd:
                 self.client.sendall('Password Confirm Failed'.encode())
+                return
             else : self.client.sendall('Sign up complete'.encode())       
             self.__Base[id] = self.__crypt.encrypt(passwd)
             self.DatabaseWrite()
             print("Register Finished")
-            break
+            return True
             
                     
        
