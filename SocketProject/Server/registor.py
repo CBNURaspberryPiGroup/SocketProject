de8fc88adc98ee4a9732d56b593b21ace9ca860a
wlans01 <76804251+wlans01@users.noreply.github.com>
2021-02-21 17:24:28 +0900

    Update registor.py

--- SocketProject/Server/registor.py
+++ SocketProject/Server/registor.py
@@ -1,90 +1,262 @@
- 
-from socket import socket
-from cryptography.fernet import Fernet as fnet
-from os.path import exists
+import socket
+import os
+import sys
+import time
+from PIL import Image
 
-class Identification:
-    def __init__(self,client):
-        self.client = client
-        if not exists('Key.key'):
-            self.__key = fnet.generate_key()
-            print(self.__key)
-            with open('Key.key','w') as f:
-                f.write(str(self.__key)[2:-1])
+##################################################
+#서버정보
+HOST = '203.227.140.199'  # The server's hostname or IP address
+PORT = 9966        # The port used by the server
+
+#파일 보내거나 받을 주소
+storage ='./'  
+#######################################################3
+
+
+
+# 디렉토리 파일 리스트
+def list_f():
+    file_list1 = os.listdir(storage)
+        
+        
+    file_list =[]
+    file_list.append([file for file in file_list1 if file.endswith(".txt")])
+    file_list.append([file for file in file_list1 if file.endswith(".png")])
+    file_list.append([file for file in file_list1 if file.endswith(".jpg")])
+
+    print ('현제 디렉토리 파일: ',file_list)
+
+
+
+
+# 명령어 해석
+
+def split():
+        split_f = filename.split(' ')  
+        
+        if split_f[0] == 'push':
+            data = client.recv(1024)
+            print(data.decode())
+            file_push(split_f)
+            data1 = client.recv(1024)
+            print(data1)
+            
+        elif split_f[0] == 'pull':
+            data = client.recv(1024)
+            print(data.decode())
+            
+            file_pull(split_f)
+            client.sandall(list_f().decode())
+        
         else:
-            with open('Key.key','r') as f:
-                self.__key = f.read().encode()
-        self.__crypt = fnet(self.__key)
-        self.DatabaseOpen()
-    
-    def DatabaseOpen(self):
-        if not exists('Auth.Data'):
-            with open('Auth.Data','w'):
-                pass
-        with open('Auth.Data','r') as f:
-            self.__Base = {}
-            try:
-                while(True):
-                    id = f.readline().encode()[:-1]
-                    psswd = f.readline().encode()[:-1]
-                    if id == b'' : break
-                    self.__Base[id] = psswd
-            except Exception:
-                pass
-        print("Database Opened")
-        print(self.__Base)
-                
-    def DatabaseWrite(self):
-        with open('Auth.Data','w') as f:
-            for id in self.__Base.keys():
-                f.write(id.decode()+'\n')
-                f.write(self.__Base[id].decode()+'\n')
-        print("Database Saved")
+            data = client.recv(1024)
+            print(data.decode())
+            
+# 파일 받기
+                  
+def file_pull(split_f):
+    filename, fileExtension = os.path.splitext(split_f[1])
         
-    def Authentification(self):
-         
-            
-            id = self.client.recv(1024)
-            if not( id in self.__Base):
-                self.client.sendall('ID Not Found'.encode())
-                return
-            
-      
-            
-            passwd = self.client.recv(1024)
-            if passwd != self.__crypt.decrypt(self.__Base[id]):
-                self.client.sendall('Wrong Password'.encode())
-                return
-           
-            else:
-                return True
-                
-            
-    def Register(self):
-        
-            
-            id = self.client.recv(1024)
-            if id in self.__Base :
-                self.client.sendall('ID Already Exists'.encode())
-                return
-            else : 
-                self.client.sendall('ID is available'.encode())
-                
-            
-        
-    
-            
-            passwd = self.client.recv(1024)
-            self.client.sendall('Password Confirm:'.encode())
-            if self.client.recv(1024) != passwd:
-                self.client.sendall('Password Confirm Failed'.encode())
-                return
-            else : self.client.sendall('Sign up complete'.encode())       
-            self.__Base[id] = self.__crypt.encrypt(passwd)
-            self.DatabaseWrite()
-            print("Register Finished")
-            return True
-            
+    if fileExtension == '.txt':
+        try:
+            f=open("%s%s"%(storage,split_f[1]),'w')
+            while True :
+                data=client.recv(1024)
+                if '\0' in data.decode():
+                    f.write(data.decode()[0:-1])
+                    print(split_f[1]+ '받기완료')
+                    break
+                else :
+                    f.write(data.decode()) 
                     
-       
+        except Exception as e:
+            print(e)               
         
+        
+    elif fileExtension == '.png'or '.jpg':
+        try:
+            
+            matadata=client.recv(1024)
+            matadata=matadata.decode()
+            matadata= matadata.split(":")
+            img_size= matadata[1].split(",")  
+            size=tuple([int(img_size[0][1:]),int(img_size[1][1:-1])])
+            img_mode=matadata[3]
+            img_data=b""
+            while True:
+                try:
+                    dat=client.recv(1024)
+                    img_data+=dat
+                    data=Image.frombytes(img_mode,size,img_data)
+                    result="ok"
+                except:
+                    result="fail"
+                if result=="ok":
+                    data.save("%s%s"%(storage,fn))
+                    break    
+
+        except Exception as e:
+            print(e)
+            
+# 파일 보내기
+            
+def file_push(split_f):
+    filename, fileExtension = os.path.splitext(split_f[1])
+    if fileExtension == '.txt':
+        send_txt(split_f)
+        
+    elif fileExtension == '.png'or '.jpg':
+        send_img(split_f)    
+    
+def send(data,size=1024):
+        client.sendall(data,size)
+        return len(data)
+    
+def send_txt(split_f):
+        try: 
+            with open(storage+"/"+split_f[1],'r') as f:
+                data = f.readlines()
+            
+            size = 0
+            for dat in data:
+                size += send(dat.encode())
+            send('\0'.encode())
+            return size
+            
+        except Exception as e:
+            print(e)
+            send(repr(e).encode())
+            
+def send_img(split_f):
+        try:
+            data = Image.open(storage+"/"+split_f[1])
+            metadata = "Size:%s:Mode:%s"%(data.size,data.mode)
+            data = data.tobytes()
+            send(metadata.encode())
+            size = 0
+            for i in range((len(data)-1)//1024+1):
+                print(i)
+                if len(data)-(i*1024) < 1024 :
+                    size += send(data[i*1024:])
+                    print('Last data N0.%s'%i)
+                else :
+                    size += send(data[i*1024:(i+1)*1024])
+                    print('data N0.%s'%i)
+                    print(size)
+            send('\0'.encode())
+            print('End')
+            return size
+        except Exception as e:
+            print(e)
+            send(repr(e).encode())                       
+        
+ 
+def reg() :
+    def condition (inf) :
+            while True :
+                inf = input(inf)
+                if " " in inf : print("공백은 사용할 수 없습니다.")
+                elif len(inf) >= 7 : print("ID는 7자 이하로 입력하세요")
+                elif len(inf) <= 2: print("ID는 3자 이상 입력하세요")                
+                else : break
+            return inf
+        
+    def process (inf) : #log in + sign up for id
+        
+            inf = condition(inf)
+            client.sendall(inf.encode())
+            inf = client.recv(1024)
+            inf = inf.decode()
+            
+            if inf == 'ID Already Exists' : 
+                print(inf)
+            else:
+                return True 
+        
+     
+    def process2 (inf) : #log in + sign up for id
+        
+            
+            condition(inf)
+            client.sendall(inf.encode())
+            inf = client.recv(1024)
+            inf = inf.decode()
+            condition(inf)
+            client.sendall(inf.encode())
+            inf = client.recv(1024)
+            inf = inf.decode()
+            if inf == 'Password Confirm Failed' : print('Password Confirm Failed')
+            else : return True
+        
+
+    
+    process('ID:')
+    if process2('PW:'):
+        return True
+
+def login ():
+    def condition (inf) :
+            while True :
+                inf = input(inf)
+                if " " in inf : print("공백은 사용할 수 없습니다.")
+                elif len(inf) >= 7 : print("ID는 7자 이하로 입력하세요")
+                elif len(inf) <= 2: print("ID는 3자 이상 입력하세요")                
+                else : break
+    
+    def process (inf) :
+        
+            condition(inf)
+            client.sendall(inf.encode())
+            inf = client.recv(1024)
+            inf = inf.decode()
+            if inf == 'Wrong Password' or inf == 'ID Not Found' : print(inf)
+            else : 
+                return True
+        
+   
+    process('ID:')
+    if process('PW:'):
+        return True
+
+
+
+    
+        
+
+def split2(se):
+    if se == 'y':
+        if login():
+            return True
+    if se == 'n':
+        if reg():
+            return True
+      
+        
+    else:
+        sys.exit()
+
+a = 0
+## 가동 ##
+while 1 :
+    a += 1
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
+    print('서버와 연결되었습니다')
+    print('ᕙ༼◕ ᴥ ◕༽ᕗ')
+    if a == 1 :
+        while True:
+            logein =input('로그인을할려면 y 회원가입을 할려면 n 을 입력하세요:')
+            client.sendall(logein.encode())
+            if split2(logein):
+                break
+    data = client.recv(1024)
+    print('서버에 있는 파일',repr(data.decode()))
+    print('୧༼◕ ᴥ ◕༽୨')
+    list_f()
+    print('ᕙ༼◕ ᴥ ◕༽ᕗ')
+    print('서버에서 파일을 받고 싶으면 pull, 업로드 하고싶을면 push르 입력후 파일명을 입력하십시오')
+    print('୧༼◕ ᴥ ◕༽୨')
+    filename = input('명령어 파일명:')
+    client.sendall(filename.encode('utf-8'))
+    split()
