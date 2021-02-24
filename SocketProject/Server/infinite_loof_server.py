ca8c7cd277602ffcb280331b0e9da31bef7ce0a4
wlans01 <76804251+wlans01@users.noreply.github.com>
2021-02-24 18:06:08 +0900

    무한가동 기능 추가

--- SocketProject/Server/infinite_loof_server.py
+++ SocketProject/Server/infinite_loof_server.py
@@ -4,9 +4,10 @@ import work
 import Send
 import Logging as Log
 from os.path import exists
+import time
 
 
-Host = ''
+Host = '1.246.117.24'
 Port = 9966
 
 storage = "./"
@@ -18,20 +19,35 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 if not exists("log.log") :
     Log.startlog()
 
-a = 0
-
-while 1 :
-    a += 1
-    server_socket.bind((Host,Port))
-    print("listening...")
-    server_socket.listen()
-    print("listened")
-    client, addr = server_socket.accept()
-    print('Connected by '+str(addr))
-    Log.log(1,"Connected by "+str(addr))
-    Com = Commmand.Command(client,storage)
-    if a == 1 : 
-        Com.split2()
-    wok=work.filelist(client,storage)
-    wok.list_f()
-    Com.split()
+server_socket.bind((Host,Port))
+def sever():
+        print("listening...")
+        server_socket.listen()
+        print("listened")
+        client, addr = server_socket.accept()
+        print('Connected by '+str(addr))
+        Log.log(1,"Connected by "+str(addr))
+        a = 0
+        while True :
+            a += 1
+            Com = Commmand.Command(client,storage)
+            if a == 1 : 
+                Com.split2()
+            wok=work.filelist(client,storage)
+        
+            wok.list_f()
+           
+            Com.split()
+           
+    
+while True:            
+    try:
+        sever()
+        
+        
+    except Exception as e:
+        
+        print(e)
+        pass
+        
+    
